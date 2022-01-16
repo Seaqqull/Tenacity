bff75f07aeddb0f7015297f19b92fe817a38c40a
Dmytro Zherdiev <seaqqullprogramming@gmail.com>
2022-01-16 20:58:17 +0100

    Update: Reference variables
     Added:
     - String variable
     - Integer variable

---
+++ Tenacity/Assets/Scripts/Properties/IntegerReference.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+
+
+namespace Tenacity.Properties
+{
+    [Serializable]
+    public class IntegerReference
+    {
+        [SerializeField] private bool UseConstant = true;
+        [SerializeField] private int ConstantValue;
+#pragma warning disable 0649
+        [SerializeField] private IntegerVariable Variable;
+#pragma warning restore 0649
+
+        public int Value
+        {
+            get { return UseConstant ? ConstantValue : Variable.Value; }
+        }
+
+
+        public IntegerReference()
+        {
+        }
+
+        public IntegerReference(int value)
+        {
+            UseConstant = true;
+            ConstantValue = value;
+        }
+
+
+        public static implicit operator int(IntegerReference reference)
+        {
+            return reference.Value;
+        }
+    }
+}
