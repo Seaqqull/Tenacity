4e33275bf533644ed8bda01291d8e94aa343b4d6
Dmytro Zherdiev <seaqqullprogramming@gmail.com>
2022-01-24 19:32:12 +0100

    Update: Collider listener
     Added:
     - ColliderListener
    
     Description:
     - [ColliderListener]: Allows to perform actions during interaction actions with colliders

---
+++ Tenacity/Assets/Scripts/Dummy/ColliderListener.cs
@@ -0,0 +1,72 @@
+using UnityEngine.Events;
+using UnityEngine;
+
+
+namespace Tenacity.Dummy
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class ColliderAction : MonoBehaviour
+    {
+        [field: SerializeField] public Collider2D Collider { get; private set; }
+        [field: SerializeField] public bool InInteraction { get; private set; }
+        [SerializeField] private Data.ColliderInteraction _onEnter;
+        [SerializeField] private Data.ColliderInteraction _onExit;
+        
+        public event UnityAction<Collider2D> OnEnter
+        {
+            add { _onEnter.AddListener(value); } 
+            remove { _onEnter.AddListener(value); }
+        }
+        public event UnityAction<Collider2D> OnExit
+        {
+            add { _onExit.AddListener(value); } 
+            remove { _onExit.AddListener(value); }
+        }
+
+
+        private void Awake()
+        {
+            if (Collider == null)
+                Collider = GetComponent<Collider2D>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            InvokeEnter(other);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            InvokeExit(other);
+        }
+
+
+        public void InvokeExit()
+        {
+            InvokeExit(null);
+        }
+        
+        public void InvokeEnter()
+        {
+            InvokeEnter(null);
+        }
+
+        public void InvokeExit(Collider2D other)
+        {
+            if (!InInteraction)
+                return;
+
+            _onExit?.Invoke(other);
+            InInteraction = false;
+        }
+        
+        public void InvokeEnter(Collider2D other)
+        {
+            if (InInteraction)
+                return;
+            
+            _onEnter?.Invoke(other);
+            InInteraction = true;
+        }
+    }
+}
